0ce4ff0347f0a3d159cc2e303ef0abf98c64e9c1
sl00p <lev@merka.us>
2022-08-03 18:49:35 -0700

    working on WriteToFile

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const inquirer = require('inquirer');
 const fs = require(`fs`);
 const {init} = require("./src/generateHTML")
+
 init();
--- src/generateHTML.js
+++ src/generateHTML.js
@@ -3,6 +3,16 @@ const { questions, manager, intern, engineer, role } = require("../data");
 const Manager = require("../lib/Manager");
 const fs = require(`fs`);
 
+function writeToFile(data) {
+  fs.writeFile("../dist/index.html", data, (err) =>
+    err ? console.error(err) : console.log("Success")
+    );
+}
+function generateHTML(data) {
+  return 
+  // HTML 
+}
+
 async function init() {
   const roster = [];
   try {
@@ -17,12 +27,14 @@ async function init() {
     console.log(roster);
     let nextEmployee = await prompt(role);
     while (nextEmployee.role != `Done?`) {
-      
+
       nextEmployee = await prompt(role);
     }
-    // writeToFile(generateHTML(answers));
+    writeToFile(generateHTML(answers));
   } catch (err) {
     console.error(err);
   }
+
 }
+
 module.exports = { init };
